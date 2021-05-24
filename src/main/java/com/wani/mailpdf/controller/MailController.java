8f96104a1c94782bc004172c89e0ecbf004e1e3c
oen142 <oen142@naver.com>
2021-05-25 00:16:45 +0900

    feat : 메일 컨트롤러 생성

--- src/main/java/com/wani/mailpdf/controller/MailController.java
+++ src/main/java/com/wani/mailpdf/controller/MailController.java
@@ -1,9 +1,20 @@
 package com.wani.mailpdf.controller;
 
+import com.wani.mailpdf.domain.MailObject;
 import com.wani.mailpdf.service.EmailService;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/mail")
 public class MailController {
 
     private final EmailService emailService;
@@ -11,4 +22,65 @@ public class MailController {
     public MailController(EmailService emailService) {
         this.emailService = emailService;
     }
+
+    @Value("${attachment.invoice}")
+    private String attachmentPath;
+
+    private static final Map<String, Map<String, String>> labels;
+
+    static {
+        labels = new HashMap<>();
+
+        //Simple email
+        Map<String, String> props = new HashMap<>();
+        props.put("headerText", "Send Simple Email");
+        props.put("messageLabel", "Message");
+        props.put("additionalInfo", "");
+        labels.put("send", props);
+
+        //Email with template
+        props = new HashMap<>();
+        props.put("headerText", "Send Email Using Text Template");
+        props.put("messageLabel", "Template Parameter");
+        props.put("additionalInfo",
+            "The parameter value will be added to the following message template:<br>" +
+                "<b>This is the test email template for your email:<br>'Template Parameter'</b>"
+        );
+        labels.put("sendTemplate", props);
+
+        //Email with attachment
+        props = new HashMap<>();
+        props.put("headerText", "Send Email With Attachment");
+        props.put("messageLabel", "Message");
+        props.put("additionalInfo",
+            "To make sure that you send an attachment with this email, change the value for the 'attachment.invoice' in the application.properties file to the path to the attachment.");
+        labels.put("sendAttachment", props);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showEmailsPage() {
+        return "emails";
+    }
+
+    @RequestMapping(value = {"/send", "/sendTemplate",
+        "/sendAttachment"}, method = RequestMethod.GET)
+    public String createMail(Model model,
+        HttpServletRequest request) {
+        String action = request.getRequestURL().substring(
+            request.getRequestURL().lastIndexOf("/") + 1
+        );
+
+        Map<String, String> props = labels.get(action);
+        Set<String> keys = props.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            model.addAttribute(key, props.get(key));
+        }
+
+        model.addAttribute("mailObject", new MailObject());
+        return "mail/send";
+    }
+
 }
